59198b4138b61d648767c8e4b8e68d400693d53f
SomeRandomGuy64 <faizan163123@gmail.com>
2023-11-11 21:51:25 +0000

    2015 Day 1 Python

---
+++ 2015/Python/Day 1/Part1.py
@@ -0,0 +1,19 @@
+# ( means santa should go up one floor
+# ) means santa should go down one floor
+# Santa starts at floor 0, the building is very tall and very deep
+
+# Start an integer at 0, read the file and store the contents into its own variable called floor_number, break that variable down character by character
+# If the character is a ( then add one to floor_number, if the character is ) then take 1 from floor_number
+
+instructions_file = open("instructions.txt", "r")
+instructions = (instructions_file.read())
+
+floor_number = 0
+
+for instruction in instructions:
+    if instruction == "(":
+        floor_number = floor_number + 1
+    elif instruction == ")":
+        floor_number = floor_number - 1
+
+print(floor_number)
---
+++ 2015/Python/Day 1/Part2.py
@@ -0,0 +1,27 @@
+# Find the position of the first character that causes Santa to enter the basement
+# The first character is position 1, second is position 2 and so on
+
+# Read the file and store the contents in a variable named instructions
+# initialize an integer variable called floor_number that represents the current floor santa is one
+# initialize an integer variable call instruction_position which represents the position of the current instruction 
+# loop through the instructions, every time the character is ( add one to floor_number, every time the character is ) take one to floor_number
+# one the floor_number reaches -1 break out of the loop and output instruction_position
+
+instructions_file = open("instructions.txt", "r")
+instructions = instructions_file.read()
+
+floor_number = 0
+instruction_position = 0
+
+for instruction in instructions:
+    if floor_number == -1:
+        break
+
+    if instruction == "(":
+        floor_number += 1
+    elif instruction == ")":
+        floor_number -= 1
+    
+    instruction_position += 1
+
+print(instruction_position)
