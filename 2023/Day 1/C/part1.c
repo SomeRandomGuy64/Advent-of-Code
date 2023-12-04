f1cfda1c7f4300cb66adfffd340d5dd02adc6d4e
SomeRandomGuy64 <faizan163123@gmail.com>
2023-12-04 23:46:00 +0000

    2023 day 1 C prep

---
+++ 2023/Day 1/C/part1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    FILE* file_ptr;
+
+    file_ptr = fopen("../input.txt", "r");
+
+    if (file_ptr == NULL) {
+        printf("file can;t be opened \n");
+    }
+
+    while (fgets) {
+
+    }
+
+    return 0;
+}
